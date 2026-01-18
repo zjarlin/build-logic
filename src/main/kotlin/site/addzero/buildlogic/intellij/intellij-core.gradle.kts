a60195b29b04548133cd744dafe774531fcdd3e6
zjarlin <zjarlin@outlook.com>
2026-01-18 12:38:19 +0800

    removal(favorites): 移除 Gradle 任务收藏功能相关代码
    - 删除了 AddToFavoritesAction 类及其相关实现
    - 删除了 FavoriteGradleTask 数据类定义
    - 删除了 GradleFavoritesService 项目级别服务
    - 删除了 GradleTaskExtractor 任务信息提取工具类
    - 删除了 RemoveFromFavoritesAction 移除收藏操作类
    - 移除了收藏功能的所有相关依赖和引用

--- src/main/kotlin/site/addzero/buildlogic/intellij/intellij-core.gradle.kts
+++ src/main/kotlin/site/addzero/buildlogic/intellij/intellij-core.gradle.kts
@@ -1,9 +1,22 @@
 package site.addzero.buildlogic.intellij
 
+import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("site.addzero.buildlogic.intellij.intellij-base")
     id("org.jetbrains.intellij.platform.module")
+}
+
+dependencies {
+    intellijPlatform {
+        bundledPlugins(
+            "com.intellij.java",
+            "org.jetbrains.kotlin",
+        )
+    }
 }
 
 
